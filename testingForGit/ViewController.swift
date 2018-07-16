da815f6031bba3e9c420fb2e98e03a6bd1ae71cc
Dylan Westfall <westf002@rangers.uwp.edu>
2018-07-16 10:30:27 -0500

    changing
    something changed

--- testingForGit/ViewController.swift
+++ testingForGit/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var value = "something"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
